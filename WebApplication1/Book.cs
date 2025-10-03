8f2dcb91d34ded5e188dc62da2e9fd46b3596d72
RubovszkiB <rubovszkib@kkszki.hu>
2025-10-03 10:32:15 +0200

    first commit

---
+++ WebApplication1/Book.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebApplication1
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public  DateTime releaseDate { get; set; }
+    }
+}
---
+++ WebApplication1/Class.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WebApplication1
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string _host;
+        private string _database;
+        private string _username;
+        private string _password;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _database = "testdb";
+            _username = "root";
+            _password = "";
+
+            string connectionString = $"Server={_host};Database={_database};Uid={_username};Pwd={_password};";
+
+           Connection = new MySqlConnection(connectionString);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
